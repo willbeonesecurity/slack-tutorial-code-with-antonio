676e5dfb64d1f09c8bd5f56c67d3f87c774990db
Christopher Hart <christopher@willbeonesecurity.xyz>
2024-09-19 14:16:43 +0200

    finished google authentication, onto password authentication

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -24,11 +24,16 @@ export const SignInCard = ({setState}: SignInCardProps) => {
     const { signIn } = useAuthActions();
     const [email, setEmail] = useState ("");
     const [password, setPassword] = useState ("");
+    const [pending, setPending] = useState(false);
 
 
-    const handleProviderSignIn = (value: "github" | "google") => {
-        signIn(value);
-    }
+    const onProviderSignIn = (value: "github" | "google") => {
+        setPending(true);
+        signIn(value)
+            .finally(() => {
+                setPending(false);
+            })
+    };
 
     return (
         <Card className="w-full h-full p-8">
@@ -43,7 +48,7 @@ export const SignInCard = ({setState}: SignInCardProps) => {
             <CardContent className="space-y-5 px-0 pb-0">
                 <form className="space-y-2.5">
                  <Input 
-                  disabled={false}
+                  disabled={pending}
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   placeholder="Email"
@@ -51,22 +56,22 @@ export const SignInCard = ({setState}: SignInCardProps) => {
                   required
                  />
                  <Input 
-                  disabled={false}
+                  disabled={pending}
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   placeholder="Password"
                   type="password"
                   required
                  />
-                 <Button type="submit" className="w-full" size="lg" disabled={false}>
+                 <Button type="submit" className="w-full" size="lg" disabled={pending}>
                     Continue
                  </Button>
                 </form>
                 <Separator />
                 <div className="flex flex-col gap-y-2.5">
                     <Button
-                        disabled={false}
-                        onClick={() => {}}
+                        disabled={pending}
+                        onClick={() => onProviderSignIn("google")}
                         variant="outline"
                         size="lg"
                         className="w-full relative"
@@ -75,8 +80,8 @@ export const SignInCard = ({setState}: SignInCardProps) => {
                         Continue With Google
                     </Button>
                     <Button
-                        disabled={false}
-                        onClick={() => handleProviderSignIn("github")}
+                        disabled={pending}
+                        onClick={() => onProviderSignIn("github")}
                         variant="outline"
                         size="lg"
                         className="w-full relative"
