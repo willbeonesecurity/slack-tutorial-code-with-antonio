428ab79cca300569b4b37729e54d1251ff3a847d
Christopher Hart <christopher@willbeonesecurity.xyz>
2024-09-25 16:31:08 +0200

    finished user information, onto workspace creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
-
 
   return (
     <div>
-      Logged in!
-
-      <Button onClick={() => signOut ()}>
-        Sign Out
-      </Button>
+      <UserButton />
     </div>
   );
 };
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -23,9 +23,10 @@ interface SignUpCardProps {
 
 export const SignUpCard = ({setState}: SignUpCardProps) => {
     const { signIn } = useAuthActions();
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [confirmPassword, setconfirmPassword] = useState ("")
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setconfirmPassword] = useState ("");
     const [error, setError] = useState("");
     const [pending, setPending] = useState(false);
 
@@ -37,7 +38,7 @@ export const SignUpCard = ({setState}: SignUpCardProps) => {
             return;
         }
         setPending(true);
-        signIn("password", { email, password, flow: "signUp"})
+        signIn("password", { name, email, password, flow: "signUp"})
             .catch (() => {
                 setError("Something has gone wrong");
             })
@@ -72,6 +73,13 @@ export const SignUpCard = ({setState}: SignUpCardProps) => {
             )}
             <CardContent className="space-y-5 px-0 pb-0">
                 <form onSubmit={onPasswordSignUp} className="space-y-2.5">
+                <Input 
+                  disabled={pending}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Full Name"
+                  required
+                 />
                  <Input 
                   disabled={pending}
                   value={email}
